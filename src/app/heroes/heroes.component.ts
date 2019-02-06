85307144b4b603d76954d807e339453aa382b978
iyerrk1811 <iyerrk1811@gmail.com>
2019-02-06 14:29:26 +1100

    hero routing elaborate

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes : Hero[];
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
   /* hero: Hero = {
     id: 1,
@@ -30,9 +30,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
+  /* onSelect(hero: Hero): void{
     this.selectedHero = hero;
-  }
+  } */
 
   getHeroes(): void {
     this.heroService.getHeroes()
